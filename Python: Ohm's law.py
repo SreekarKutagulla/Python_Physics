2018d0fd9b43feeacab82b7b2dd9ed7326d4ed96
SreekarKutagulla <42827269+SreekarKutagulla@users.noreply.github.com>
2019-02-17 16:30:40 -0800

    Update Python: Ohm's law.py

--- Python: Ohm's law.py
+++ Python: Ohm's law.py
@@ -1,66 +1,42 @@
-class ohm(object):
-    def __init__(self,r,i,v):
-        self.r=r
-        self.i=i
-        self.v=v
 
-    def get_volt(self): 
-        "Get the Value of Voltage"
-        if self.r ==0 or self.i==0:
-            return "Values must be greater than '0' "
-        else:
-            return self.r*self.i
+eye=0
 
-    def get_current(self):
-        "Get the value of Current"
-        if self.v ==0 or self.r==0:
-            return "Values must be greater than '0' "
-        else:
-            return self.v/self.r
+###############################
+r= input('Enter Resistance Value , Press x if no Value : ')
 
-    def get_resistance(self):
-        "Get the Value of Resistance"
-        if self.v==0 or self.i==0:
-            return "Values must be greater than '0' "
-        else:
-            return self.v/self.i
+if r != str("x"):
+    r = float(r)
+if r == str("x") :
+    eye = eye + 1
+    
+#############################
 
+i= input('Enter Current Value , Press x if no Value : ')
+if i != str("x"):
+    i = float(i)
+if i == str("x") :
+    eye = eye + 1
 
+#############################
 
+v= input('Enter Voltage Value , Press Enter x if no Value : ')
+if v != str("x"):
+    v = float(v)
+if v == str("x") :
+    eye = eye + 1
 
-eye=0 
-try:
-    r=float(input('Enter Resistance Value , Press Enter if no Value : '))
-except:
-    eye+=1
-try:
-    i=float(input('Enter Current Value , Press Enter if no Value : '))
-except:
-    eye+=1
-try:
-    v=float(input('Enter Voltage Value , Press Enter if no Value : '))
-except:
-    eye+=1
+#################################
 
-if eye>=2 :
-    print ('ERRORR: Enter atlest Two Values')
+if eye > 1 :
+    print ('ERROR: Please enter only one x')
+
+if v == "x":
+        print(str(float( r * i)) + " Volts")
+
+if i == "x":
+        print(str(float(v/r)) + " Amps")
 
 
-try :
-    if r and i :
-        v=0
-        print ('Voltage = ',ohm(r,i,v).get_volt(),'Volt')
-except :
-    pass
-try:
-    if v and r:
-        i=0
-        print ('Current = ',ohm(r,i,v).get_current(),'Amp')
-except :
-    pass
-try:
-    if v and i:
-        r=0
-        print ('Resistance = ',ohm(r,i,v).get_resistance(),'Ohm')
-except :
-    pass
+if r == "x":
+        print (str(float(v/c)) + " Ohms")
+
